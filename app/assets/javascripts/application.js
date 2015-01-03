00ca1f504b18dbd7bc8fc67ef355cbdf5b79dbd5
Oskar Niburski <oskarniburski@gmail.com>
2015-01-03 18:46:55 -0500

    Update of view and proepr post handle

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,6 +36,24 @@ $(document).ready(function() {
         dataType: "json",
         data: {events: allData },
         complete: function(data) {
+          $('.all-output').html('')
+          var jsonData = data.responseJSON['events']
+          var htmlString = '<table><tr><th>Approve/Unapprove</th><th>Name</th><th>Approved</th><th>Location</th><th>Price</th><th>Latitude</th><th>Longitude</th><th>Url</th><th>Image</th><th>DayOn</th><th>DayEnd</th></tr>'
+          $.each(jsonData, function(index, eventArr) {
+            htmlString += '<tr><td><input id="event" name="event" type="checkbox" value="'+eventArr['ruby_id']+'"></td>'
+            + '<td><label class="check-box" for="event">'+eventArr['name']+'</label></td>'
+            +'<td>'+eventArr['approved']+'</td>'
+            +'<td>'+eventArr['location']+'</td>'
+            +'<td>'+eventArr['latitude']+'</td>'
+            +'<td>'+eventArr['longitude']+'</td>'
+            +'<td>'+eventArr['url']+'</td>'
+            +'<td>'+eventArr['image']+'</td>'
+            +'<td>'+eventArr['dayOn']+'</td>'
+            +'<td>'+eventArr['dayEnd']+'</td>'
+            +'</tr>'
+          })
+          htmlString += '</tr>'
+          $('.all-output').html(htmlString)
         }
       })
     }
