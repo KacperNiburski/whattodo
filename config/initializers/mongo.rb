e4a05c1c0b7fc994d3680c1948a680aff315b1d7
Oskar Niburski <oskarniburski@gmail.com>
2015-02-12 20:13:35 -0500

    Better mongo connect

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,4 +1,11 @@
-MongoMapper.connection = Mongo::Connection.new('10.8.0.1', 27017)
+
+db = URI.parse('mongodb://10.8.0.1:27017/map')
+db_name = db.path.gsub(/^\//, '')
+@db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+@db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.password.nil?)
+@db_connection
+
+MongoMapper.connection = Mongo::Connection.new('10.8.0.1', 27017).db(db_name)
 MongoMapper.database = "map"
 
 if defined?(PhusionPassenger)
