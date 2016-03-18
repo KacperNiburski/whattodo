8680ebd1160974b5246a12500378f7ab0e0f8cc8
Oskar Niburski <oskarniburski@gmail.com>
2016-03-18 09:25:55 -0400

    Uhm

--- app/assets/javascripts/react/components/button.es6.jsx
+++ app/assets/javascripts/react/components/button.es6.jsx
@@ -1,24 +1,24 @@
-class Button extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      text: this.props.text
+const Button = React.createClass({
+  getInitialState: function() {
+    return {
+      text: this.props.text,
     }
-  }
+  },
 
   questionLevelDummy() {
     this.props.increaseQuestionLevel()
-  }
+    console.log("Inside component")
+    console.log(this.props.text);
+    this.setState({text: parseInt(this.state.text) + 1})
+  },
 
   render () {
+    console.log(this.props.text);
+    console.log(this.state.text);
     return (
       <div>
-        <div onClick={this.questionLevelDummy.bind(this)}>Text: {this.state.text}</div>
+        <div onClick={this.questionLevelDummy}>Text: {this.state.text}</div>
       </div>
     );
   }
-}
-
-Button.propTypes = {
-  text: React.PropTypes.string
-};
+})
